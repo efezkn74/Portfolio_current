1ec67a47bcb0f6ba49dfcc4c08861d6d13957097
Efe Özkan <efe.oezkan@student.reutlingen-university.de>
2025-05-12 03:26:00 +0200

    testauth_end..

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env._NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env._NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('SUPABASE_URL und/oder SUPABASE_ANON_KEY fehlen in der .env-Datei!');
